83d683aff9766bc7115e1b6b7845c9fc47a6b4f1
johndevops1 <john.boothapati@sage.com>
2022-09-20 12:18:10 +0100

    test

--- arithmatic.sh
+++ arithmatic.sh
@@ -18,3 +18,4 @@ b=20.33
 echo "$a+$b" | bc
 #-------------------------------------------Floating numnber with division numbers desimal points------------------#
 echo "scale=2;$a/$b" | bc
+#This is end
